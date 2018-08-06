f3cff9c75cfb49a678ec4bfa009c0a2f232e5fe1
arkady <arkasha90@gmail.com>
2018-08-06 15:43:33 +0300

    add reverse proxy for phantomjs

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 )
 
@@ -28,6 +30,23 @@ func main() {
 		log.Printf("new request: %s", r.RemoteAddr)
 		index(token, w, r)
 	})
+
+	http.HandleFunc("/phantom", func(w http.ResponseWriter, r *http.Request) {
+		u, _ := url.Parse("http://127.0.0.1:4444")
+		tokenEnv := os.Getenv("token")
+		token := r.URL.Query().Get("token")
+
+		if token != tokenEnv {
+			w.WriteHeader(400)
+			w.Write([]byte("bad token"))
+			log.Println("bad token")
+			return
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(u)
+		proxy.ServeHTTP(w, r)
+	})
+
 	err := http.ListenAndServe(":6677", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
